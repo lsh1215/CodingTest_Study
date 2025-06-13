bfa24564a914775b47b0ae59ade0266776a081ba
leesanghun <vitash1215@gmail.com>
2025-06-13 21:37:37 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.57 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,11 +1,20 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
+        System.out.println(arr1.length);
+        System.out.println(arr2.length);
+        System.out.println(arr1[0].length);
+        
         int row = arr1.length;
         int col = arr2[0].length;
         int common = arr2.length;
 
         int[][] answer = new int[row][col];
-
+        
+        // arr1[0][0] * arr2[0][0] + 
+        // arr1[0][1] * arr2[1][0] +
+        
+        // arr1[0][0] * arr2[1][0] + 
+        // arr1[0][1] * arr2[1][1] +
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 for (int k = 0; k < common; k++) {
