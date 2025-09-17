5b2fba1827e5c50364009265d0fdc9a755b3f8ff
leesanghun <vitash1215@gmail.com>
2025-09-17 09:22:38 +0900

    [level 1] Title: 시저 암호, Time: 1.73 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            
+            if (c == ' ') {
+                answer.append(c);
+                continue; 
+            }
+            
+            if (Character.isLowerCase(c)) {
+                char shifted = (char) ('a' + (c - 'a' + n) % 26);
+                answer.append(shifted);
+            } 
+            else if (Character.isUpperCase(c)) {
+                char shifted = (char) ('A' + (c - 'A' + n) % 26);
+                answer.append(shifted);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
