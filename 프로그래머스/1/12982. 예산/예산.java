1820b7b73400a5f448eea62724b6459bc300719a
leesanghun <vitash1215@gmail.com>
2025-09-19 11:04:08 +0900

    [level 1] Title: 예산, Time: 0.60 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for (int request : d) {
+            if (budget >= request) {
+                budget -= request;
+                answer++;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
