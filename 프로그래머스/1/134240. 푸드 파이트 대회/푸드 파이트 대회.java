8b521ab28b85456590246e3c3681520003a662a7
leesanghun <vitash1215@gmail.com>
2025-09-22 10:40:31 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 18.46 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(i);
+            }
+        }
+        String leftStr = sb.toString();
+        String rightStr = sb.reverse().toString();
+        
+        return leftStr + "0" + rightStr;
+    }
+}
