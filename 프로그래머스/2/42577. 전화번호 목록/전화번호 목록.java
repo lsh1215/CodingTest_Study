8d839c703f53e521da78bb36c16a2c08d9cd1762
leesanghun <vitash1215@gmail.com>
2026-02-05 11:35:12 +0900

    [level 2] Title: 전화번호 목록, Time: 292.67 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,24 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        int n = phone_book.length;
-        
         Arrays.sort(phone_book);
-        
-        for(int i=0; i<n-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
+  
+        for(int i = 1; i < phone_book.length; i++){
+            
+            if(phone_book[i].startsWith(phone_book[i-1])){
                 return false;
             }
-            // for(int j=i+1; j<n; j++){
-            //     if(phone_book[j].startsWith(phone_book[i])){
-            //         return false;
-            //     }
-            // }
         }
-        // for(String phone : phone_book){
-        //     System.out.println(phone);
-        // }
+        
         return true;
     }
-    
 }
