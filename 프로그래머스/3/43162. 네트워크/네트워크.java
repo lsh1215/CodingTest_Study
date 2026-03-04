47b3fc56cce601fd2d24629dcfae9f2dd26bdf5a
leesanghun <vitash1215@gmail.com>
2026-03-04 23:53:34 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,9 +1,23 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
+    public static boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        int count = 0;
+        
+        visited = new boolean[n];
+        
+        for(int i=0; i<n; i++){
+            if(visited[i] == false){
+                bfs(i, computers);
+                count++;
+            }
+        }
+        
+        return count;
+    }
     
-    public static void bfs(int x, int[][] computers) {
+    public void bfs(int x, int[][] computers){
         Queue<Integer> queue = new ArrayDeque<>();
         visited[x] = true;
         queue.offer(x);
@@ -11,29 +25,12 @@ class Solution {
         while(!queue.isEmpty()){
             int start = queue.poll();
             
-            for(int i=0; i<computers.length; i++){
-                if(computers[start][i] == 1 && visited[i] == false){
-                    queue.offer(i);
-                    visited[i] = true;
+            for(int j=0; j<computers.length; j++){
+                if(computers[start][j] == 1 && visited[j] == false){
+                    queue.offer(j);
+                    visited[j] = true;
                 }
             }
         }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        // boolean[] visited = new boolean[n];
-        visited = new boolean[n];
-        
-        int count = 0;
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                bfs(i, computers);
-                count++;
-            }
-        }
-        
-        
-        return count;
     }
 }
